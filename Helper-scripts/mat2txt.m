6df95286a7e4f1ebaf661f21976d70d11c33fd7c
christophert314 <ctseng6@emory.edu>
2015-06-29 11:52:19 -0400

    Update mat2txt.m

--- Helper-scripts/mat2txt.m
+++ Helper-scripts/mat2txt.m
@@ -1,10 +1,5 @@
-%Modification from go_all.m from DREAM5 evaluation script
-DIR = '../data';
-methods = directory_list(DIR);
-method = methods{1};
-%%%%%
-load(DIR '_fMRI_method_' method '.mat');
-outfile = {DIR '_fMRI_method_' method '.txt'};
+load('testfile.mat');
+outfile = {'testfile.txt'};
 header={1:size(LFP)};
 fid = fopen(outfile, 'w');
 fprintf(fid, '%d\t', header{:});
