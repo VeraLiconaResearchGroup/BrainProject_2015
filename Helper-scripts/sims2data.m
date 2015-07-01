174a1a58951970941dfa4dd3fac880c5eb25d1ed
christophert314 <ctseng6@emory.edu>
2015-07-01 13:50:41 -0400

    Update sims2data.m

--- Helper-scripts/sims2data.m
+++ Helper-scripts/sims2data.m
@@ -2,7 +2,7 @@
 function sims2data(simname)
 sims = load([simname '.mat']);
   for i=1:sims.Nsubjects
-    LFP = sims.ts(((i-1)*200)+1:(i*200),:)';
+    LFP = sims.ts(((i-1)*sims.Ntimepoints)+1:(i*sims.Ntimepoints),:)';
     Connectivity = squeeze(sims.net(i,:,:));
     Params.fs = 0.500;
     save([simname,'_N',num2str(sims.Nnodes),'T',num2str(sims.Ntimepoints),'_', num2str(i), '.mat'], 'LFP', 'Connectivity', 'Params');
