4532fa23cdded9bcffa845af49604369643b97bd
christophert314 <ctseng6@emory.edu>
2015-07-01 11:45:42 -0400

    Update sims2data.m

--- Helper-scripts/sims2data.m
+++ Helper-scripts/sims2data.m
@@ -1,7 +1,9 @@
-function sims = sims2data(sims)
+%Converts the NetSim data into a MULAN time series dataset
+sims2data(sims)
   for i=1:sims.Nsubjects
-    sim.LFP = sims.ts(int32(i-1)*200,:)';
-    sim.Connectivity = sims.net(i,:,:);
-    sim.Params = zeros(1,1);
-    sim.Params.fs = 0.500;
+    LFP = sims.ts(int32(i-1)*200,:)';
+    Connectivity = sims.net(i,:,:);
+    Params = zeros(1,1);
+    Params.fs = 0.500;
+    save(sims '_' i '.mat', 'LFP', 'Connectivity', 'Params');
   end
