7e17407003b4974dfc9f512ad679f0cec02d2846
christophert314 <ctseng6@emory.edu>
2015-07-13 12:52:48 -0400

    Update genData.m

--- Helper-scripts/genData.m
+++ Helper-scripts/genData.m
@@ -1,10 +1,11 @@
+%% Converts ICA time series into format for MULAN and runs CalEvaN using a dummy Connectivity matrix.
 function genData(filename, foldername)
     fileID=fopen(['datafolder/', filename]);
-    header  = textscan(fileID, '%s', 1);
-    LFP = textscan(fileID, ['%f', repmat('%f',[1,48])], 'CollectOutput', 1);
+    header  = textscan(fileID, '%s', 49);
+    LFP = textscan(fileID, ['%f', repmat('%f',[1,48])], 204);
     LFP = cell2mat(LFP);
     LFP = LFP';
-    Connectivity = zeros(49,49);
+    Connectivity = ones(49,49);
     Params.fs = 0.500;
-    save([foldername, '/data/', foldername,'fmriCS100S1N49.mat'], 'LFP', 'Connectivity', 'Params');
-    mln_CalEvaN([foldername], [foldername], 'MULAN/GenerateData/structureN49L0', 'nmmParams', 49, 1, 204, 1, 'fMRI');
+    save([foldername, '/data/', foldername,'fmriCS100S1N204.mat'], 'LFP', 'Connectivity', 'Params');
+    mln_CalEvaN([foldername],[foldername], 'GenerateData/structureN49L0', 'nmmParams', '49', '1', '204', '1', 'fMRI');
