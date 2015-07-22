9fcdf5a62391556ceaee471ba9440e26c8c52882
Shi Chao <sw9@outlook.com>
2015-07-21 21:20:27 -0400

    fixed bug with multisub

--- Helper-scripts/multisub_sims.m
+++ Helper-scripts/multisub_sims.m
@@ -5,51 +5,53 @@ sims = load([simname '.mat']);
 edges = int32(nnz(squeeze(sims.net(1,:,:))) - sims.Nnodes);
 strname = ['GenerateData/structureN' int2str(sims.Nnodes) 'L' int2str(edges) '.mat']
 if ~exist([strname], 'file')
-  PGS = cell(1);
-  matrix = [];
-  counter = 1;
-  Connectivity = squeeze(sims.net(1,:,:));
-  for i=1:sims.Nnodes;
-      for j=1:sims.Nnodes;
-          if Connectivity(i,j)>0
-              matrix(2,counter)=i;
-              matrix(1,counter)=j;
-              counter = counter + 1;
-         end
-     end
-  end
-  PGS{1,1} = matrix;
-  save(strname, 'PGS');
+    PGS = cell(1);
+    matrix = [];
+    counter = 1;
+    Connectivity = squeeze(sims.net(1,:,:));
+    for i=1:sims.Nnodes;
+        for j=1:sims.Nnodes;
+            if Connectivity(i,j)>0
+                matrix(2,counter)=i;
+                matrix(1,counter)=j;
+                counter = counter + 1;
+            end
+        end
+    end
+    PGS{1,1} = matrix;
+    save(strname, 'PGS');
 end
 
 %Convert the remaining time series
-  extraZero = '00';
-  for i=str2num(start):str2num(finish)
+extraZero = '00';
+for i=str2num(start):str2num(finish)
     if i < 100 & i > 9
         extraZero = '0';
     elseif i >= 100
         extraZero = '';
     end
-   foldername = [simname, '_Sub', extraZero, num2str(i)];
-   mkdir(foldername);
-   dir = foldername;
-   mkdir(dir, 'AUC');
-   mkdir(dir, 'data');
-   mkdir(dir, 'Results');
-   mkdir(dir, 'ToutResults');
-   
-   LFP = sims.ts(((i-1)*sims.Ntimepoints)+1:(i*sims.Ntimepoints),:)';
-   Connectivity = squeeze(sims.net(i,:,:));
-   Params.fs = 0.500;
-   save([foldername, '/data/', simname,'_Sub', extraZero, num2str(i), 'fmriCS100S1N', num2str(sims.Ntimepoints), '.mat'], 'LFP', 'Connectivity', 'Params');
-  end
-  parfor i=str2num(start):str2num(finish)
+    foldername = [simname, 'Sub', extraZero, num2str(i)];
+    mkdir(foldername);
+    dir = foldername;
+    mkdir(dir, 'AUC');
+    mkdir(dir, 'data');
+    mkdir(dir, 'Results');
+    mkdir(dir, 'ToutResults');
+    
+    LFP = sims.ts(((i-1)*sims.Ntimepoints)+1:(i*sims.Ntimepoints),:)';
+    Connectivity = squeeze(sims.net(i,:,:));
+    Params.fs = 0.500;
+    save([foldername, '/data/', foldername, 'fmriCS100S1N', num2str(sims.Ntimepoints), '.mat'], 'LFP', 'Connectivity', 'Params');
+end
+
+parfor i=str2num(start):str2num(finish)
     extraZero = '00';
     if i < 100 & i > 9
         extraZero = '0';
     elseif i >= 100
         extraZero = '';
     end
-   foldername = [simname, '_Sub', extraZero, num2str(i)];
-   mln_CalEvaN(foldername,foldername, ['GenerateData/structureN' int2str(sims.Nnodes) 'L' int2str(edges) '.mat'], 'nmmParams', num2str(sims.Nnodes), '1', num2str(sims.Ntimepoints), '1', 'fMRI');
-  end
+
+    fname = [simname, 'Sub', extraZero, num2str(i)];
+    mln_CalEvaN(fname, fname, ['GenerateData/structureN' int2str(sims.Nnodes) 'L' int2str(edges) '.mat'], 'nmmParams', num2str(sims.Nnodes), '1', num2str(sims.Ntimepoints), '1', 'fMRI');
+end
