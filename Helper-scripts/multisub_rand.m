53ce628e3af7c10e069fbe5c1e4fc2605923e6a4
christophert314 <ctseng6@emory.edu>
2015-08-03 18:31:01 -0400

    Add comments

--- Helper-scripts/multisub_rand.m
+++ Helper-scripts/multisub_rand.m
@@ -8,12 +8,16 @@
 
 function multisub_rand(start, finish)
   extraZero = '00';
+  
+  % Iterate through random network edge lists
   for i=str2num(start):str2num(finish)
     if i < 100 && i > 9
         extraZero = '0';
     elseif i >= 100
         extraZero = '';
     end
+    
+    % Extract edge lists
     fileID = fopen(['randnetedges/rand_' extraZero int2str(i) '.txt']);
     orig = textscan(fileID, '%d %d');
     fclose(fileID);
@@ -26,6 +30,8 @@ function multisub_rand(start, finish)
     nodes2 = cell2mat(orig(2));
     PGS = cell(1);
     matrix = zeros(2,edges);
+    
+    % Generate MULAN structure file from random network edge list
     for n=1:edges(i,1)
         matrix(2,n) = int32(nodes1(n,1)+1);
         matrix(1,n) = int32(nodes2(n,1)+1);
@@ -33,6 +39,8 @@ function multisub_rand(start, finish)
     PGS{1,1} = matrix;
     save(['GenerateData/structure_rand' int2str(i) 'N' int2str(nodes(i,1)) 'L' int2str(edges(i,1)) '.mat'], 'PGS');
   end
+  
+  % Run MULAN with random network structure file
   parfor i=str2num(start):str2num(finish)
      extraZero = '00';
      if i < 100 && i > 9
