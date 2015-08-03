89bd0e10d41844f74e0452b96d163fc9e8debcdf
christophert314 <ctseng6@emory.edu>
2015-08-03 19:37:18 -0400

    Add comments

--- Helper-scripts/multisub_gen.m
+++ Helper-scripts/multisub_gen.m
@@ -1,3 +1,12 @@
+%% Evaluate MULAN results
+%% Parameters
+% name: name of directory with MULAN results
+% first: # of first subject
+% last: # of last subject
+% time: length of time series
+%% Example execution: multisub_gen sim4 1 50 200
+% Note: multisub_gen will iterate through every consecutive subject number from the start value to the finish value
+
 function multisub_gen(name, start, finish, time)
     extraZero = '00';
     for i=str2num(start):str2num(finish)
