abd2b4c0a24f22bab1303809c0a7d8b432155d44
christophert314 <ctseng6@emory.edu>
2015-08-03 11:07:13 -0400

    Add comments

--- Helper-scripts/edgelist2str.m
+++ Helper-scripts/edgelist2str.m
@@ -1,8 +1,10 @@
-%For converting an edgelist exported from an R igraph into a structure file
-%edgelist = filename of an edgelist, with the head nodes in the first column and corresponding tail nodes in the second
-%nodes = number of nodes
-%edges = number of edges
-function PGS = edgelist2str(edgelist, nodes, edges)
+%% For converting an edgelist exported from an R igraph into a structure file
+%% Parameters
+% edgelist = filename of an edgelist, with the head nodes in the first column and corresponding tail nodes in the second
+% nodes = number of nodes
+% edges = number of edges
+%% Example execution: edgelist2str edgelist 50 80
+function edgelist2str(edgelist, nodes, edges)
 
 fileID = fopen(edgelist);
 orig = textscan(fileID, '%d %d');
