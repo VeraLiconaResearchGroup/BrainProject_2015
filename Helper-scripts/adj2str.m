69e6054d1c45b35d93943c891577c41d2e467ad9
christophert314 <ctseng6@emory.edu>
2015-08-03 11:05:51 -0400

    Add comments

--- Helper-scripts/adj2str.m
+++ Helper-scripts/adj2str.m
@@ -1,7 +1,9 @@
-%Converts an adjacency matrix into an edgelist and saves as a MULAN structure file
-%adj = name of n x n adjacency matrix
-%nodes = number of nodes
-%edges = number of edges
+%% Converts an adjacency matrix into an edgelist and saves as a MULAN structure file
+%% Parameters
+% adj: name of n x n adjacency matrix
+% nodes: number of nodes
+% edges: number of edges
+%% Example execution: adj2str adjmatrix 50 80
 function adj2str(adj, nodes, edges)
 
 PGS = cell(1);
@@ -9,7 +11,7 @@ matrix = zeros(2,edges);
 counter = 1;
 for i=1:nodes
     for j=1:nodes
-        if adj(i,j)>0 & i ~= j
+        if adj(i,j)~=0 & i~=j
             matrix(2,counter)=i;
             matrix(1,counter)=j;
             counter = counter + 1;
