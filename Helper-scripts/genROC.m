c918d4deebf48e6b2d1ed02bc613a9b038dfccc0
christophert314 <ctseng6@emory.edu>
2015-08-03 11:22:15 -0400

    Add comments

--- Helper-scripts/genROC.m
+++ Helper-scripts/genROC.m
@@ -1,6 +1,8 @@
-% Generate ROC curves for all methods.
-% Meths = Meths struct from the AUC file saved in the AUC folder after each run.
-% AUC file will typically be in the format: AUC_[folder name]fmriCS100S1N[length of time series].mat
+%% Generate ROC curves for all methods
+%% Parameters
+% Meths = Meths struct from the AUC file saved in the AUC folder after each run
+%% Example execution: genROC Meths
+% Note: AUC file will typically be in the format: AUC_[folder name]fmriCS100S1N[length of time series].mat
 % Meths is extracted by loading that AUC file and then typing in the command line: genROC Meths
 function genROC(Meths)
   x = Meths.Fpr;
