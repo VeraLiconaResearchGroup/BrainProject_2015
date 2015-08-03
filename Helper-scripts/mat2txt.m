0d5692b38ffc11c37b3a16656725034425a90194
christophert314 <ctseng6@emory.edu>
2015-08-03 10:55:22 -0400

    Add comments

--- Helper-scripts/mat2txt.m
+++ Helper-scripts/mat2txt.m
@@ -1,6 +1,8 @@
-%Converts the time series from a MULAN dataset into a DREAM5 software compatible format
-load('testfile.mat');
-outfile = {'testfile.txt'};
+% Converts the time series from a MULAN dataset into a DREAM5 software compatible format
+% filename = name of MULAN mat file to be converted into txt file
+function mat2txt(filename)
+load([filename, '.mat']);
+outfile = [filename, '.txt'];
 header={1:size(LFP)};
 fid = fopen(outfile, 'w');
 fprintf(fid, '%d\t', header{:});
